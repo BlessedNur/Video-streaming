aaaa5d3bf5e44c98c2bc477f5f61684c0151ded1
Blessed-Nur <blessednur67@gamil.com>
2024-05-14 04:06:18 +0100

    Database connected

--- src/Utility/db.js
+++ src/Utility/db.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables from .env file
-
+dotenv.config();
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGO;
-    
+
     if (!mongoURI) {
       throw new Error("MONGO environment variable is not set");
     }
 
-    console.log("Attempting to connect to MongoDB with URI:", mongoURI); // Log the connection string
-    
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
